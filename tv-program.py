9febda89fd6330fd6ad264dbea9bd5d553979889
Rei Kaizen <bondenie@gmail.com>
2023-05-13 21:16:53 +0800

    pseudocode for class tv

---
+++ tv-program.py
@@ -0,0 +1,17 @@
+#TV requirements:
+# ON and Off
+# channel switcher
+# volume control
+
+#define a class named TV
+    #create function to initialize channel, volumeLevel, and on as TV default object
+    #function to turn the TV on
+    #function to turn the TV off
+    #function to get the channel
+    #function to set the channel
+    #function to get the volume
+    #function to set the volume
+    #function to increase the channel
+    #function to decrease the channel
+    #function to increase the volume
+    #function to decrease the volume
